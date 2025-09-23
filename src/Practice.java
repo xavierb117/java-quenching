e4ed61d8d31e163cf9922a8385b9016bf1e404cb
Auberon López <auberonedu@gmail.com>
2025-09-23 08:40:47 -0700

    More questions

--- src/Practice.java
+++ src/Practice.java
@@ -42,5 +42,87 @@ public class Practice {
         return null;
     }
 
-    
+    /**
+     * Returns the biggest number in a linked list.
+     * 
+     * @param head the head of the linked list
+     * @return the biggest number in the list
+     * @throws IllegalArgumentException if head is null
+     */
+    public static int biggestNumber(ListNode<Integer> head) {
+        return 0;
+    }
+
+    /**
+     * Returns a frequency map counting how frequently items appear in a linked list.
+     * 
+     * Example:
+     *   Input: a -> x -> a -> a -> x -> y
+     *   Output: {a:3, x:2, y: 1}
+     * 
+     * Returns an empty map if head is null
+     * 
+     * @param <T> the type of data held by the list
+     * @param head the head of the list
+     * @return a frequency map of values in the list
+     */
+    public static <T> Map<T, Integer> frequencies(ListNode<T> head) {
+        return null;
+    }
+
+
+    /**
+     * Returns the number of levels in the tree.
+     * 
+     * An empty tree has 0 levels, a tree with only a root has 1 level.
+     * 
+     * @param root the root of the tree
+     * @return the number of levels in the tree
+     */
+    public static int levelCount(BinaryTreeNode<?> root) {
+        return 0;
+    }
+
+
+    /**
+     * Returns the sum at a specified level in a binary tree.
+     * 
+     * For example, if the given level was 3:
+     *       5
+     *     /   \
+     *    8     4
+     *   / \   / 
+     *  7  9  2
+     *    /
+     *   1
+     * 
+     * Nodes at level 3: 7, 9, and 2
+     * Sum of nodes at level 3: 18 
+     * 
+     * The root is considered to be at level 1.
+     * 
+     * Returns 0 if the tree is empty or if the level is not present in the tree.
+     * 
+     * @param root the root of the binary tree
+     * @param level the level to sum
+     * @return the sum of the nodes at the given level
+     */
+    public static int sumAtLevel(BinaryTreeNode<Integer> root, int level) {
+        return 0;
+    }
+
+
+    /**
+     * Returns true if the sum of the values in a given tree is equal to the sum
+     * of the values in the given list. 
+     * 
+     * An empty tree or list is considered to have a sum of 0.
+     * 
+     * @param root The root of the binary tree
+     * @param head The head of the linked list
+     * @return true if the sums are equal, false otherwise
+     */
+    public static boolean sumMatch(BinaryTreeNode<Integer> root, ListNode<Integer> head) {
+        return false;
+    }
 }
