54ce73f527c2956c57b67c9d7745b1f9fe389f82
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-09-24 20:02:26 -0700

    Finished adults

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -83,7 +84,19 @@ public class Practice {
      * @throws NullPointerException if ages is null
      */
     public static Set<String> adults(Map<String, Integer> ages) {
-        return null;
+        if (ages == null) {
+            throw new NullPointerException();
+        }
+        
+        Set<String> adults = new HashSet<>();
+
+        for (String name : ages.keySet()) {
+            if (ages.get(name) >= 18) {
+                adults.add(name);
+            }
+        }
+        
+        return adults;
     }
 
     /**
